4303eea5345eba653a2430296079cc3ef27ec30d
Rogos-LSGR <slawasyper@ya.ru>
2021-05-20 11:57:25 +0300

    db

--- bd.sql
+++ bd.sql
@@ -1,1 +1,176 @@
-SELECT * FROM mydb.teachers;
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: mydb
+-- ------------------------------------------------------
+-- Server version	8.0.23-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `complite`
+--
+
+DROP TABLE IF EXISTS `complite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `complite` (
+  `id_test` int NOT NULL,
+  `id_student` int NOT NULL,
+  `estimation` int NOT NULL,
+  PRIMARY KEY (`id_test`,`id_student`),
+  KEY `fk_complite_student_idx` (`id_student`),
+  CONSTRAINT `fk_complite_student` FOREIGN KEY (`id_student`) REFERENCES `students` (`id`),
+  CONSTRAINT `fk_complite_test` FOREIGN KEY (`id_test`) REFERENCES `tests` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `complite`
+--
+
+LOCK TABLES `complite` WRITE;
+/*!40000 ALTER TABLE `complite` DISABLE KEYS */;
+INSERT INTO `complite` VALUES (1,1,4),(2,1,5);
+/*!40000 ALTER TABLE `complite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `school`
+--
+
+DROP TABLE IF EXISTS `school`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `school` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `school`
+--
+
+LOCK TABLES `school` WRITE;
+/*!40000 ALTER TABLE `school` DISABLE KEYS */;
+INSERT INTO `school` VALUES (1,'ЖОПА НоМЕр 1');
+/*!40000 ALTER TABLE `school` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lostname` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `patronymic` varchar(255) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `id_teacher` int DEFAULT NULL,
+  `class` varchar(32) NOT NULL,
+  `id_school` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_students_school_idx` (`id_school`),
+  KEY `fk_students_teacher_idx` (`id_teacher`),
+  CONSTRAINT `fk_students_school` FOREIGN KEY (`id_school`) REFERENCES `school` (`id`),
+  CONSTRAINT `fk_students_teacher` FOREIGN KEY (`id_teacher`) REFERENCES `teachers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Жопен','Жопа','Жопович','88005553535','jopa@jopa.jp','jopa',1,'j5',1);
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teachers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lostname` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `patronymic` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `id_school` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_teachers_shool_idx` (`id_school`),
+  CONSTRAINT `fk_teachers_shool` FOREIGN KEY (`id_school`) REFERENCES `school` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teachers`
+--
+
+LOCK TABLES `teachers` WRITE;
+/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
+INSERT INTO `teachers` VALUES (1,'Жопен','Жопа','Жопович','88005553535','jopa@jopa.jp','jopa',1),(2,'f','ff','ff','435','fg','f',1);
+/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tests`
+--
+
+DROP TABLE IF EXISTS `tests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tests` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `class` varchar(32) NOT NULL,
+  `src` varchar(255) NOT NULL,
+  `id_teacher` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_tests_teacher_idx` (`id_teacher`),
+  CONSTRAINT `fk_tests_teacher` FOREIGN KEY (`id_teacher`) REFERENCES `teachers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tests`
+--
+
+LOCK TABLES `tests` WRITE;
+/*!40000 ALTER TABLE `tests` DISABLE KEYS */;
+INSERT INTO `tests` VALUES (1,'jopa','j5','public/tests/Жопович.txt\n',1),(2,'pis','j5','gfhgf',1),(3,'her','j5','ghf',1),(4,'herrre','j5','ssdf',2),(5,'666','6','dgf',1);
+/*!40000 ALTER TABLE `tests` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-20 11:55:18
